78e11e492952aafbd1a20a377ec98cc15f72ec6b
Manuel DeJesus <69539934+ManuelLovell@users.noreply.github.com>
2024-02-04 16:47:55 -0800

    Link Fix

--- src/utilities.ts
+++ src/utilities.ts
@@ -175,7 +175,7 @@ export function GetWhatsNewButton()
         }
         await OBR.modal.open({
             id: Constants.EXTENSIONWHATSNEW,
-            url: `/bsWhatsNew.html`,
+            url: `/bswhatsnew.html`,
             height: 500,
             width: 350,
         });
